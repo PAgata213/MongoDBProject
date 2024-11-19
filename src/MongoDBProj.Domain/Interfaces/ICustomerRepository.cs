42d31a38951df80f8a6887caf5f9f6b885d62f5b
Patryk <ricochet852@gmail.com>
2024-11-19 15:05:22 +0100

    Add domain project

---
+++ src/MongoDBProj.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using MongoDBProj.Domain.Models;
+
+namespace MongoDBProj.Domain.Interfaces;
+public interface IMovieRepository
+{
+	public Task<ICollection<Movie>> GetMovies();
+	public ValueTask<Movie> GetMovieById(Guid movieId);
+	public ValueTask<Guid> CreateMovie(Movie movie);
+	public Task UpdateMovie(Movie movie);
+	public Task DeleteMovie(Guid movieId);
+}
---
+++ src/MongoDBProj.Domain/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MongoDBProj.Domain.Models;
+public record Movie
+{
+	public Guid Id { get; init; } = Guid.NewGuid();
+
+	public string? Name { get; set; }
+
+	public string? Description { get; set; }
+
+	public string? Title { get; set; }
+}
