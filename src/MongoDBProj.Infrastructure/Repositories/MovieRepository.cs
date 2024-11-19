bac387d4520d58a1a2017c532a1d5c21f684d943
Patryk <ricochet852@gmail.com>
2024-11-19 15:05:50 +0100

    Add infrastructure project

---
+++ src/MongoDBProj.Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using MongoDBProj.Domain.Interfaces;
+using MongoDBProj.Domain.Models;
+
+namespace MongoDBProj.Infrastructure.Repositories;
+internal class MovieRepository : IMovieRepository
+{
+	public ValueTask<Guid> CreateMovie(Movie movie) => throw new NotImplementedException();
+	public Task DeleteMovie(Guid movieId) => throw new NotImplementedException();
+	public ValueTask<Movie> GetMovieById(Guid movieId) => throw new NotImplementedException();
+	public Task<ICollection<Movie>> GetMovies() => throw new NotImplementedException();
+	public Task UpdateMovie(Movie movie) => throw new NotImplementedException();
+}
